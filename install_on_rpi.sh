5261f99424cf1512baf00eb81cafec5d144140cc
AJJ <alexander@jaworowski.se>
2018-09-14 10:09:54 +0000

    Make some changed to the install script, most must be done manually

--- install_on_rpi.sh
+++ install_on_rpi.sh
@@ -1,13 +1,14 @@
-#!/usr/bin/bash
+#!/bin/bash
 #What is needed in order to execute simple BA commands on wavelet:
 #- update pip
 wget https://bootstrap.pypa.io/get-pip.py
-sudo "python get-pip.py --prefix=/usr/local/"
+sudo python get-pip.py --prefix=/usr/local/
 sudo pip install virtualenv
 #- create virtualenv i ba_com, will be 2.7...
 virtualenv venv
-source venv/bin/activate
+echo  Do the rest manually.
+#source venv/bin/activate
 #- at this point, with pyton3 as 3.2, skip requirements.txt, install only pyserial
-pip install pyserial
+#pip install pyserial
 #- Should be able to run cmd.py
-./cmd.py -p /dev/ttyAMA0 version
+#./cmd.py -p /dev/ttyAMA0 version
